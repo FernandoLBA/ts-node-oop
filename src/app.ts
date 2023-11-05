30806bd4223ccb7cf389ae3a9aa629de99270263
“fernandolba“ <fbarrios.pyc@gmail.com>
2023-11-05 15:47:58 -0500

    agregando configuración de middlewares, creación de métodos en la calse App

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,77 @@
 import express, { Application } from "express";
-import { API_VERSION, NODE_ENV, PORT } from "./config/config";
-import { Routes } from "./interfaces/route.interface";
-import { logger } from "./utils/logger";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import displayRoutes from "express-routemap";
+import helmet from "helmet";
+import hpp from "hpp";
+import morgan from "morgan";
+
+import { API_VERSION, LOG_FORMAT, NODE_ENV, PORT } from "./config/config";
+import corsConfig from "./config/cors.config";
+import { Routes } from "./interfaces/route.interface";
+import { logger, stream } from "./utils/logger";
 
 class App {
   public app: Application;
   public env: string;
   public port: number;
+  public server: any;
 
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || "development";
     this.port = Number(PORT) || 3000;
 
+    this.connectToDatabase();
+    this.initializeMiddlewares();
     this.initializeRoutes(routes);
+    this.initializeSwagger();
+    this.initializeErrorHandling();
+  }
+
+  /**
+   *
+   * @returns
+   */
+  public getServer() {
+    return this.app;
+  }
+
+  /**
+   *
+   * @param done
+   */
+  public closeServer(done?: any) {
+    this.server = this.app.listen(this.port, () => {
+      done();
+    });
+  }
+
+  /**
+   *
+   */
+  private connectToDatabase() {
+    // TODO: inicializar la conexión
+  }
+
+  /**
+   *
+   */
+  private initializeMiddlewares() {
+    // * Permite visualizar las peticiones HTTP
+    this.app.use(morgan(LOG_FORMAT ?? "../logs", { stream }));
+    // * Permite la conexión entre dominios
+    this.app.use(cors(corsConfig));
+    // * Permite ***
+    this.app.use(hpp());
+    // * Brinda mayor seguridad a la API, al evitar inyecciones en los headers
+    this.app.use(helmet());
+    // * Permite transformar el body en un json
+    this.app.use(express.json());
+    // * Permite el soporte de urlencoded
+    this.app.use(express.urlencoded({ extended: true }));
+    // * Permite el manejo de cookies
+    this.app.use(cookieParser());
   }
 
   /**
@@ -23,9 +79,6 @@ class App {
    * @param routes
    */
   public initializeRoutes(routes: Routes[]) {
-    this.app.use(cors());
-    this.app.use(express.json());
-
     // * Se iteran las rutas y se asignan a la app
     routes.forEach((route) => {
       this.app.use(`/api/${API_VERSION}`, route.router);
@@ -45,6 +98,14 @@ class App {
       logger.info(`🔗 http://localhost:${this.port}/api/${API_VERSION} 🔗`);
       logger.info("==================================");
     });
+  }
+
+  private initializeSwagger() {
+    // TODO: init swagger
+  }
+
+  private initializeErrorHandling() {
+    // TODO: configure error handling
   }
 }
 
